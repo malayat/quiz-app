0d3b3333f1f9d87584adefbaddeb7f4b174487e0
Alejandro Ayala <alejandro.ayala@solmedia.ec>
2015-07-08 22:11:39 -0500

    Branch creditos. Add commit Exercise P2P Module 6

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var creditosController = require('../controllers/creditos_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,5 +11,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+router.get('/author', creditosController.creditos);
 
 module.exports = router;
